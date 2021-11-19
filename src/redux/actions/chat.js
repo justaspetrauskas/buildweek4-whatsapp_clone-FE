2bec2dc71768395c25228b7dc3e8f790bbe228a2
js25s <petrauskas.justas@gmail.com>
2021-11-19 14:03:10 +0100

    updates

--- src/redux/actions/chat.js
+++ src/redux/actions/chat.js
@@ -4,7 +4,7 @@ const setInitSoketStore = (payload) => ({
 });
 
 export const setInitSocket=()=>{
-    return {
+    return (dispatch) => {
         let token = "Bearer " + window.localStorage.getItem("user_Token");
         const ADDRESS = process.env.REACT_APP_API_BE;
         const socket = io(ADDRESS, {
@@ -12,9 +12,10 @@ export const setInitSocket=()=>{
           query: {
             authorization: token.split(" ")[1],
           },
-          dispatch(setInitSoketStore(socket))
-    }
-
+          
+    },
+    dispatch(setInitSoketStore(socket))
+}
 }
 
 // gets fired when the main app is rendering for the first time after login: 
